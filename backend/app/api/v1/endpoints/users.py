351163a4d7ee7b1d1fba953abb630c47ed280439
t.tatiyev <t.tatiyev@trafficwave.kz>
2023-04-12 01:44:07 +0600

    add endpoint

---
+++ backend/app/api/v1/endpoints/users.py
@@ -0,0 +1,32 @@
+import fastapi.security.oauth2
+
+from ... import controllers
+from ... import responses
+from ... import schemas
+
+
+router = fastapi.APIRouter()
+
+
+@router.post(
+    '/register',
+    summary='Register new user',
+    response_model=schemas.UserGet,
+    response_description='Obtained token',
+    responses=responses.gen_responses([controllers.TokenObtainAPIResponseUnauthenticated]),
+)
+async def register(
+    user_data: schemas.UserRegistration,
+):
+    """Register new user:
+
+    - `phone`: Mobile phone number
+    - `email`: Provide username
+    - `password`: Provide user password
+
+    The following status codes are defined for 401 response:
+
+    * `token_invalid_credentials` - Invalid credentials were provided
+    """
+
+    return await controllers.user_create(user_data)
