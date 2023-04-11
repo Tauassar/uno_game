062bc217ba8280975098c472f5260b8514323a43
t.tatiyev <t.tatiyev@trafficwave.kz>
2023-04-11 23:04:25 +0600

    HOTFIX: migrations

--- backend/app/migrations/postgres/env.py
+++ backend/app/migrations/postgres/env.py
@@ -4,9 +4,9 @@ import logging.config
 import alembic.context
 import sqlalchemy.ext.asyncio
 
-import uno.api.models  # noqa: F401
-import uno.core.conf
-import uno.core.postgres
+import app.api.models  # noqa: F401
+import app.core.conf
+import app.core.postgres
 
 
 config = alembic.context.config
@@ -18,7 +18,7 @@ logging.config.fileConfig(config.config_file_name)
 def apply_migrations(connection):
     alembic.context.configure(
         connection=connection,
-        target_metadata=uno.core.postgres.metadata,
+        target_metadata=app.core.postgres.metadata,
     )
     with alembic.context.begin_transaction():
         alembic.context.run_migrations()
@@ -26,7 +26,7 @@ def apply_migrations(connection):
 
 async def run_migrations():
     engine = sqlalchemy.ext.asyncio.create_async_engine(
-        url=uno.core.conf.postgres.uri,
+        url=app.core.conf.postgres.uri,
     )
 
     async with engine.begin() as connection:
