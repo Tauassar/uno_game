7a6f0fa6901cfef722823e9465e41b514279a14b
t.tatiyev <t.tatiyev@trafficwave.kz>
2023-04-17 23:13:33 +0600

    HOTFIX: basic websocket connection setup

---
+++ backend/app/api/v1/endpoints/websocket.py
@@ -0,0 +1,58 @@
+import asyncio
+import logging
+from datetime import datetime
+from typing import Union
+
+import fastapi.security.oauth2
+from fastapi import WebSocket, WebSocketDisconnect
+
+
+router = fastapi.APIRouter()
+logger = logging.getLogger(__name__)
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.connections.append(websocket)
+
+    async def broadcast(self, data: Union[dict, str]):
+        for connection in self.connections:
+            await connection.send_json(data)
+
+
+manager = ConnectionManager()
+
+
+@router.websocket(
+    '/game/{game_session_id}',
+)
+async def websocket_endpoint(
+    websocket: WebSocket,
+    game_session_id: str,
+):
+    await manager.connect(websocket)
+
+    while True:
+        try:
+            # Receive the JSON data sent by a client.
+            data = await websocket.receive_json()
+            # Some (fake) heavey data processing logic.
+            message_processed = {
+                **data,
+                'processed': True,
+                'game_session_id': game_session_id,
+            }
+            # Send JSON data to the client.
+            await manager.broadcast(
+                {
+                    "message": message_processed,
+                    "time": datetime.now().strftime("%H:%M:%S"),
+                }
+            )
+        except WebSocketDisconnect:
+            logger.info("The connection is closed.")
+            break
--- backend/app/api/v1/routers.py
+++ backend/app/api/v1/routers.py
@@ -3,9 +3,11 @@ import fastapi
 from .endpoints import dummies
 from .endpoints import token
 from .endpoints import users
+from .endpoints import websocket
 
 
 api_router = fastapi.APIRouter()
 api_router.include_router(dummies.router, tags=['dummies'])
 api_router.include_router(token.router, tags=['token'])
 api_router.include_router(users.router, tags=['users'])
+api_router.include_router(websocket.router, tags=['websocket'])
